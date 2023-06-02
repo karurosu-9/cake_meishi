9ba0248df599bb40d414ddd3d4fd46f4db0b0fe9
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-02 09:49:42 +0900

    新規マイグレーションファイルの作成

---
+++ config/Migrations/20230602003904_CreateDivisions.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateDivisions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('divisions');
+        $table->addColumn('division_name', 'string', [
+            'limit' => 100,
+            'null' => false,
+        ])
+        ->addColumn('created', 'datetime')
+        ->addColumn('modified', 'datetime');
+        $table->addIndex('division_name', ['unique' => 'true']);
+        $table->create();
+    }
+}
