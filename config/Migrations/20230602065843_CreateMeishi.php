87442d63fb92d49410fd89680aecf934fb3728d1
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-02 17:42:07 +0900

    migrationファイルとseederファイルの作り直し

---
+++ config/Migrations/20230602065843_CreateMeishi.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateMeishi extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('meishi');
+        $table->addColumn('corp_id', 'integer', [
+            'limit'=> 255,
+            'null' => false,
+        ])
+        ->addColumn('division', 'string', [
+            'limit' => 255,
+            'null' => false,
+        ])
+        ->addColumn('title', 'string', [
+            'limit' => 50,
+            'null' => true,
+        ])
+        ->addColumn('employee_name', 'string', [
+            'limit' => 50,
+            'null' => false,
+        ])
+        ->addColumn('address', 'string', [
+            'limit' => 255,
+            'null' => false,
+        ])
+        ->addColumn('tel', 'string', [
+            'limit' => 13,
+            'null' => true,
+        ])
+        ->addColumn('created', 'datetime')
+        ->addColumn('modified', 'datetime');
+        $table->create();
+    }
+}
