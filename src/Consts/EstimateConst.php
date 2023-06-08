6a2226e4160563f59fa83a2866c388c159ae1a74
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-08 11:23:38 +0900

    EstimateのForm用のマジックナンバーの作成

---
+++ src/Consts/EstimateConst.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+namespace App\Consts;
+
+class EstimateConst
+{
+    //見積作成画面の補足記入以外の欄の数
+    const FORM_NOT_HOSOKU = '5';
+    
+    //見積作成画面の補足記入欄の数
+    const FORM_HOSOKU = '3';
+}
+?>
