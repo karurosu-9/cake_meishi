5dca43399346e1590ae569ef538e4255b1ec3fb9
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-09 18:46:52 +0900

    add, confirmEstimate, resultEstimateの編集

--- src/Controller/EstimateController.php
+++ src/Controller/EstimateController.php
@@ -71,9 +71,17 @@ class EstimateController extends AppController
 
     public function confirmEstimate()
     {
+        //年月日の取得
+        $year = date('Y');
+        $month = date('n');
+        $date = date('d');
+
+        //見積作成時の日付を"-"の表示から年月日の表示にする
+        $currentDate = $year . '年' . $month . '月' . $date . '日';
 
         $corp = $this->Corps->find();
         $myCorp = $this->MyCorps->find()->first();
+
         //郵便番号をハイフン有りで出力する
         $postCode = substr($myCorp->post_code, 0, 3) . '-' . substr($myCorp->post_code, 3);
 
@@ -91,6 +99,10 @@ class EstimateController extends AppController
 
         if ($this->request->is('post')) {
 
+            $postData = $this->request->getData();
+            //post送信されたフォームの値をセッションへ格納
+            $this->request->getSession()->write('postData', $postData);
+
             $corp = $corp->where(['id' => $this->request->getData('corp_id')])->first();
 
             //postで飛んできた値を取得
@@ -107,6 +119,7 @@ class EstimateController extends AppController
         }
 
         $data = [
+            'currentDate' => $currentDate,
             'corp' => $corp,
             'postCode' => $postCode,
             'tel' => $tel,
@@ -130,23 +143,20 @@ class EstimateController extends AppController
 
     public function resultEstimate()
     {
-        //var_dump($this->request->getData());exit;
+
+        //セッションに格納されたフォームの値を$postDataへ格納
+        $postData = $this->request->getSession()->read('postData');
+        //セッションに格納された値を削除
+        $this->request->getSession()->delete('postData');
+
         $estimate = $this->Estimate->newEmptyEntity();
         if ($this->request->is('post')) {
-            $total_amount = 0;
-            $estimate = $this->Estimate->patchEntity($estimate, $this->request->getData());
-
-            //total_amountに合計金額を格納
-            for ($i = 1; $i <= EstimateConst::FORM_NOT_HOSOKU; $i++) {
-                $total_amount += $estimate['amount' . $i];
-            }
-
-            $estimate['total_amount'] = $total_amount;
+            $estimate = $this->Estimate->patchEntity($estimate, $postData);
 
             if ($this->Estimate->save($estimate)) {
                 $this->Flash->success(__('The estimate has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['action' => 'view']);
             }
             $this->Flash->error(__('The estimate could not be saved. Please, try again.'));
         }
