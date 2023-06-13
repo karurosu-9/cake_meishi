26113dda79f4314e5640390820007f9a8505e811
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-13 20:08:02 +0900

    アクション内のコードの追加と修正

--- src/Controller/EstimatesController.php
+++ src/Controller/EstimatesController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -23,6 +24,13 @@ class EstimatesController extends AppController
 
         //自社の情報を取得
         $this->MyCorps = $this->getTableLocator()->get('MyCorps');
+
+        $this->paginate = [
+            'limit' => 20,
+            'order' => [
+                'created' => 'DESC',
+            ],
+        ];
     }
     /**
      * Index method
@@ -31,12 +39,30 @@ class EstimatesController extends AppController
      */
     public function index()
     {
-        $this->paginate = [
-            'contain' => ['Corps'],
-        ];
-        $estimates = $this->paginate($this->Estimates);
 
-        $this->set(compact('estimates'));
+        $estimatesCount = 0;
+
+        $estimates = [];
+
+        $corpId = $this->request->getData('corp_id');
+
+        $corps = $this->Corps->find('list', ['limit' => 200, 'valueField' => 'corp_name']);
+
+        if ($this->request->is('post')) {
+            $estimates = $this->Estimates->find('all')->contain(['Corps']);
+            $estimates = $estimates->where(['corp_id' => $corpId]);
+            $estimatesCount = $estimates->count();
+            $estimates = $this->paginate($estimates);
+        }
+
+
+        $data = [
+            'corps' => $corps,
+            'estimates' => $estimates,
+            'estimatesCount' => $estimatesCount,
+        ];
+
+        $this->set($data);
     }
 
     /**
@@ -67,7 +93,16 @@ class EstimatesController extends AppController
         $conditions = $myCorp->conditions;
         $deadline = $myCorp->deadline;
 
+        //見積データの取得
+        $result = $this->processEstimateData($id);
 
+        $tekiyo = $result['tekiyo'];
+        $unitPrice = $result['unit_price'];
+        $quantity = $result['quantity'];
+        $amount = $result['amount'];
+        $note = $result['note'];
+        $totalAmount = $result['total_amount'];
+        $hosoku = $result['hosoku'];
 
         $data = [
             'estimate' => $estimate,
@@ -81,6 +116,13 @@ class EstimatesController extends AppController
             'place' => $place,
             'conditions' => $conditions,
             'deadline' => $deadline,
+            'tekiyo' => $tekiyo,
+            'unitPrice' => $unitPrice,
+            'quantity' => $quantity,
+            'amount' => $amount,
+            'note' => $note,
+            'totalAmount' => $totalAmount,
+            'hosoku' => $hosoku,
         ];
 
         $this->set($data);
@@ -93,6 +135,7 @@ class EstimatesController extends AppController
      */
     public function add()
     {
+
         $corps = $this->Corps->find('list', ['limit' => 200, 'valueField' => 'corp_name'])->all();
 
         //入力したフォームデータの内容をsessionへ格納してconfirmEstimateアクションへ渡す
@@ -184,6 +227,8 @@ class EstimatesController extends AppController
     public function confirmEstimate()
     {
 
+        $loginUser = $this->Authentication->getResult()->getData();
+
         $estimate = $this->Estimates->newEmptyEntity();
 
 
@@ -226,17 +271,17 @@ class EstimatesController extends AppController
         $quantity = $result['quantity'];
         $amount = $result['amount'];
         $note = $result['note'];
-        $totalAmount = number_format($result['totalAmount']);
+        $totalAmount = $result['total_amount'];
         $hosoku = $result['hosoku'];
 
         //フォームの内容がOKであった場合のpostLinkボタンを押された時の処理
         if ($this->request->is('post')) {
             //sessionへ格納されたフォームの値を$postDataへ格納
             $postData = $this->request->getSession()->read('postData');
-            //var_dump($postData);exit;
             $estimate = $this->Estimates->patchEntity($estimate, $postData);
-            $estimate['total_amount'] = (int)$totalAmount;
+            $estimate['total_amount'] = $totalAmount;
             $estimate['date'] = $currentDate;
+            $estimate['create_user'] = $loginUser->user_name;
             if ($this->Estimates->save($estimate)) {
                 $this->Flash->success(__('見積データを登録しました。'));
                 $this->request->getSession()->delete('postData');
@@ -315,7 +360,7 @@ class EstimatesController extends AppController
         return $this->redirect(['action' => 'index']);
     }
 
-    private function processEstimateData()
+    /*private function processEstimateData()
     {
         $postData = $this->request->getSession()->read('postData');
 
@@ -361,5 +406,74 @@ class EstimatesController extends AppController
             'hosoku' => $hosoku,
             'totalAmount' => $totalAmount,
         ];
+    }*/
+
+    private function processEstimateData($id = null)
+    {
+
+            $tekiyo = [];
+            $unitPrice = [];
+            $quantity = [];
+            $amount = [];
+            $note = [];
+            $hosoku = [];
+            $totalAmount = 0;
+
+        //セッションから値を取得する時の処理
+        if ($this->request->getSession()->read('postData')) {
+
+            $postData = $this->request->getSession()->read('postData');
+
+            for ($i = 1; $i <= EstimateConst::FORM_NOT_HOSOKU; $i++) {
+                $tekiyo[$i] = $postData['tekiyo' . $i];
+                $unitPrice[$i] = $postData['unit_price' . $i];
+                $quantity[$i] = $postData['quantity' . $i];
+                $amount[$i] = $postData['amount' . $i];
+                $note[$i] = $postData['note' . $i];
+                $totalAmount += (int)$amount[$i];
+            }
+
+
+            for ($i = 1; $i <= EstimateConst::FORM_HOSOKU; $i++) {
+                $hosoku[$i] = $postData['hosoku' . $i];
+            }
+
+
+            return [
+                'tekiyo' => $tekiyo,
+                'unit_price' => $unitPrice,
+                'quantity' => $quantity,
+                'amount' => $amount,
+                'note' => $note,
+                'hosoku' => $hosoku,
+                'total_amount' => $totalAmount,
+            ];
+        } else {
+
+            $estimate = $this->Estimates->find()->where(['id' => $id])->first()->toArray();
+
+            for ($i = 1; $i <= EstimateConst::FORM_NOT_HOSOKU; $i++) {
+                $tekiyo[$i] = $estimate['tekiyo' . $i];
+                $unitPrice[$i] = $estimate['unit_price' . $i];
+                $quantity[$i] = $estimate['quantity' . $i];
+                $amount[$i] = $estimate['amount' . $i];
+                $note[$i] = $estimate['note' . $i];
+                $totalAmount += (int)$amount[$i];
+            }
+
+            for ($i = 1; $i <= EstimateConst::FORM_HOSOKU; $i++) {
+                $hosoku[$i] = $estimate['hosoku' . $i];
+            }
+
+            return [
+                'tekiyo' => $tekiyo,
+                'unit_price' => $unitPrice,
+                'quantity' => $quantity,
+                'amount' => $amount,
+                'note' => $note,
+                'hosoku' => $hosoku,
+                'total_amount' => $totalAmount,
+            ];
+        }
     }
 }
