688370cf2a89f173e5208f725bc8e692f3de55e4
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-14 19:47:37 +0900

    h()メソッドの追加とunit_price1とamount1は記入必須に変更

--- templates/Estimates/add.php
+++ templates/Estimates/add.php
@@ -2,13 +2,6 @@
 use App\Consts\EstimateConst;
 ?>
 
-<style>
-    span {
-        font-weight: bold;
-        font-size: 20px;
-    }
-</style>
-
 <div class="estimate content">
     <h1><?= __('Estimate') ?></h1>
     <br>
@@ -34,19 +27,26 @@ use App\Consts\EstimateConst;
             <th><?= __('Amount') ?></th>
             <th><?= __('Note') ?></th>
         </tr>
-        <?php for ($i = 1; $i <= EstimateConst::FORM_NOT_HOSOKU; $i++): ?>
+        <tr>
+            <td><?= $this->Form->text('tekiyo1', ['style' => 'width:200px', 'value' =>h($this->request->getData('tekiyo1'))] ) ?></td>
+            <td>￥<?= $this->Form->text('unit_price1', ['style' => 'width:85px', 'id' => 'unit_price1', 'onChange' => 'keisan()', 'value' => h($this->request->getData('unit_price1')), 'required' => true]) ?></td>
+            <td><?= $this->Form->text('quantity1', ['style' => 'width:50px', 'id' => 'quantity1', 'onChange' => 'keisan()',  'value' => h($this->request->getData('quantity1')), 'required' => true]) ?></td>
+            <td>￥<?= $this->Form->text('amount1', ['style' => 'width:100px', 'id'=> 'amount1', 'readonly' => true, 'value' => h($this->request->getData('amount1'))]) ?></td>
+            <td><?= $this->Form->text('note1', ['style' =>'width:400px', 'value' => h($this->request->getData('note1'))]) ?></td>
+        </tr>
+        <?php for ($i = 2; $i <= EstimateConst::FORM_NOT_HOSOKU; $i++): ?>
             <tr>
-                <td><?= $this->Form->text('tekiyo' . $i, ['style' => 'width:200px', 'value' => $this->request->getData('tekiyo' . $i)] ) ?></td>
-                <td>￥<?= $this->Form->text('unit_price' . $i, ['style' => 'width:85px', 'id' => 'unit_price' . $i, 'onChange' => 'keisan()', 'value' => $this->request->getData('unit_price' . $i)]) ?></td>
-                <td><?= $this->Form->text('quantity' . $i, ['style' => 'width:50px', 'id' => 'quantity' . $i, 'onChange' => 'keisan()',  'value' => $this->request->getData('quantity' . $i)]) ?></td>
-                <td>￥<?= $this->Form->text('amount' . $i, ['style' => 'width:100px', 'id'=> 'amount' . $i, 'readonly' => true, 'value' => $this->request->getData('amount' . $i)]) ?></td>
-                <td><?= $this->Form->text('note' . $i, ['style' =>'width:400px', 'value' => $this->request->getData('note' . $i)]) ?></td>
+                <td><?= $this->Form->text('tekiyo' . $i, ['style' => 'width:200px', 'value' => h($this->request->getData('tekiyo' . $i))] ) ?></td>
+                <td>￥<?= $this->Form->text('unit_price' . $i, ['style' => 'width:85px', 'id' => 'unit_price' . $i, 'onChange' => 'keisan()', 'value' => h($this->request->getData('unit_price' . $i))]) ?></td>
+                <td><?= $this->Form->text('quantity' . $i, ['style' => 'width:50px', 'id' => 'quantity' . $i, 'onChange' => 'keisan()',  'value' => h($this->request->getData('quantity' . $i))]) ?></td>
+                <td>￥<?= $this->Form->text('amount' . $i, ['style' => 'width:100px', 'id'=> 'amount' . $i, 'readonly' => true, 'value' => h($this->request->getData('amount' . $i))]) ?></td>
+                <td><?= $this->Form->text('note' . $i, ['style' =>'width:400px', 'value' => h($this->request->getData('note' . $i))]) ?></td>
             </tr>
         <?php endfor; ?>
     </table>
     <h3><?= __('Insert Hosoku') ?></h3>
     <?php for ($i = 1; $i <= EstimateConst::FORM_HOSOKU; $i++): ?>
-        <?= $this->Form->text('hosoku' . $i, ['value' => $this->request->getData('hosoku' . $i)]) ?>
+        <?= $this->Form->text('hosoku' . $i, ['value' => h($this->request->getData('hosoku' . $i))]) ?>
     <?php endfor; ?>
     <br>
     <br>
