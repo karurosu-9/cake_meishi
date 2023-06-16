599b587ac52007770d2a1e88a73cb31717989426
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-16 14:36:58 +0900

    検索用へルーパーメソッドの追加

--- src/View/Helper/CommonHelper.php
+++ src/View/Helper/CommonHelper.php
@@ -9,6 +9,7 @@ class CommonHelper extends Helper
     public function initialize(array $config) :void
     {
         parent::initialize($config);
+        $this->Form = $this->_View->loadHelper('Form');
     }
 
     public function displayNoDataMessage($counter)
@@ -17,6 +18,18 @@ class CommonHelper extends Helper
             return '<span style="color: red; font-weight: bold;">※データは存在しません</span>';
         }
         return;
+    }
+    //検索フォーム用ヘルパーメソッド
+    public function SearchForm($data)
+    {
+        $formContent = "";
+
+        $formContent .= $this->Form->create($data);
+        $formContent .= $this->Form->control('keyword');
+        $formContent .= $this->Form->button(__('Search'));
+        $formContent .= $this->Form->end();
+
+        return $formContent;
     }
 }
 ?>
