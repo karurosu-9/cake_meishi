95d1208aa2b4d6fd5b3a2945f8a09dab1b9f62c4
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-16 11:20:28 +0900

    jsコードをesimate.jsファイルに移動

--- templates/Estimates/edit.php
+++ templates/Estimates/edit.php
@@ -1,5 +1,7 @@
 <?php
 use App\Consts\EstimateConst;
+
+echo $this->Html->script('estimate.js');
 ?>
 
 <div class="estimate content">
@@ -53,27 +55,3 @@ use App\Consts\EstimateConst;
     </div>
     <?= $this->Form->end() ?>
 </div>
-
-<script>
-    function keisan() {
-    for (let i = 1; i <= 5; i++) {
-        //単価の値を取得
-        let unit_price = document.getElementById('unit_price' + i).value;
-        //数量の値を取得
-        let quantity = document.getElementById('quantity' + i).value;
-        //.valueを付けると値を取得しようとする為、valueは不要
-        let amountElement = document.getElementById('amount' + i);
-
-        if (unit_price !== '' && quantity !== '') {
-            if (isFinite(quantity)) {
-                let addAmount = unit_price * quantity;
-                amountElement.value = addAmount;
-            } else {
-                amountElement.value = unit_price;
-            }
-        } else {
-            amountElement.value = "";
-        }
-    }
-}
-</script>
