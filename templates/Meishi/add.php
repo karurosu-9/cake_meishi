7e7eb7bf02aae6fa2b9a86ffb39875a110ab0e8e
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-21 17:21:06 +0900

    登録、編集フォームをヘルパーメソッドから表示に変更

--- templates/Meishi/add.php
+++ templates/Meishi/add.php
@@ -2,22 +2,26 @@
     <h1><?= __('Meishi Register') ?></h1>
     <br>
     <br>
-    <?php
-    echo $this->Form->create($meishi);
-    echo $this->Form->control('corp_id', [
-        'options' => $corps,
-
-    ]);
-    echo $this->Form->control('division');
-    echo $this->Form->control('title');
-    echo $this->Form->control('employee_name');
-    echo $this->Form->control('tel');
-    echo $this->Form->button(__('Register'));
-    echo $this->Form->end();
-    ?>
+    <?= $this->Common->meishiForm($meishi, $corps, 'add') ?>
     <br>
     <br>
     <div style="font-size: 20px">
         <?= $this->Html->link(__('<< Back'), ['controller' => 'Meishi', 'action' => 'index']) ?>
     </div>
 </div>
+
+
+<?php
+/*
+echo $this->Form->create($meishi);
+echo $this->Form->control('corp_id', [
+    'options' => $corps,
+    'required' => true,
+]);
+echo $this->Form->control('division');
+echo $this->Form->control('title');
+echo $this->Form->control('employee_name');
+echo $this->Form->control('tel');
+echo $this->Form->button(__('Register'));
+echo $this->Form->end();*/
+?>
--- templates/Meishi/edit.php
+++ templates/Meishi/edit.php
@@ -2,15 +2,8 @@
     <h1><?= __('Meishi Edit') ?></h1>
     <br>
     <br>
-    <?php
-    echo $this->Form->create($meishi);
-    echo $this->Form->control('division');
-    echo $this->Form->control('title');
-    echo $this->Form->control('employee_name');
-    echo $this->Form->control('tel');
-    echo $this->Form->button(__('Register'));
-    echo $this->Form->end();
-    ?>
+    <h3>〘 <?= h($meishi->corp->corp_name) ?> 〙</h3>
+    <?= $this->Common->meishiForm($meishi, null, 'edit') ?>
     <br>
     <br>
     <div style="font-size: 20px">
