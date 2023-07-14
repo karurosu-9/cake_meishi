21f221a2542f44cbb06a4e190c98fa9484d257ab
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-14 15:10:45 +0900

    パスワードの変更ボタンを条件によって表示／非表示 するよう設定

--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -2,9 +2,11 @@
     <h1><?= __('User Edit') ?></h1>
     <br>
     <br>
+    <?php if ($loginUser->id === $user->id || $loginUser->admin === '管理者' || $loginUser->admin === 'システム') : ?>
     <div class="button">
         <?= $this->Html->link(__('Change Password'), ['action' => 'changePassword', $user->id]) ?>
     </div>
+    <?php endif; ?>
     <br>
     <br>
     <?= $this->ViewForm->generateUserForm($user, $divisionsList, 'edit') ?>
